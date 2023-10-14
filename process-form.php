8579cfec30ce40ad067cef8d0180dedb3a1a9d57
Stian Brandal <stianbrandal@live.com>
2023-10-14 19:37:35 +0200

    Add files via upload

--- process-form.php
+++ process-form.php
@@ -7,19 +7,19 @@ $feeling = filter_input(INPUT_POST,"feeling",FILTER_VALIDATE_INT);
 $terms = filter_input(INPUT_POST, "terms", FILTER_VALIDATE_BOOL); //$_POST("terms");
 
 
-
 if( ! $terms){
     die("Please complett traing");
 }
-//var_dump($ovelse,$sets,$reps,$terms,$feeling);// ,$ovelse, $set, $reps,$_types,
+//var_dump($ovelse1,$sets1,$reps1,$feeling1);// ,$ovelse, $set, $reps,$_types,
 
-$host = "localhost";
-$dbname = "trening_db";
-$username ="root";
-$password = "";
+$host = "sql11.freesqldatabase.com";
+$dbname = "sql11653374";
+$username ="sql11653374";
+$password = "aeqbuXwQq2";
+$portNr = "3306";
 
 
-$conn =mysqli_connect($host, $username,$password, $dbname);
+$conn =mysqli_connect($host, $username,$password, $dbname,$portNr);
 
 
 if (mysqli_connect_errno()){
@@ -29,8 +29,10 @@ if (mysqli_connect_errno()){
 
 $sql = "INSERT INTO trening (ovelse, sets, reps, feeling)
         VALUES(?,?,?,?)";
-
 $stmt = mysqli_stmt_init($conn);
+
+
+
 
 if ( ! mysqli_stmt_prepare($stmt,$sql)){
     die(mysqli_error($conn));
---
+++ readme.txt
@@ -0,0 +1,7 @@
+
+
+SEINARE I PROSJEKET :
+
+https://www.youtube.com/watch?v=wHspfWWn1II&ab_channel=OnlineITtutsTutorials
+
+
